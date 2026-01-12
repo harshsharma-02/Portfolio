0556845302423cb0234868549283529caaf80177
harshsharma-02 <harshdeos7@gmail.com>
2026-01-12 15:41:38 +0530

    changes in UI

--- src/components/Heroimg.jsx
+++ src/components/Heroimg.jsx
@@ -1,22 +1,21 @@
 import "./HeroimgStyle.css";
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import bg2 from "../Assets/img/bg2.jpg"
 import { FaDownload } from "react-icons/fa";
 
 const Heroimg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img  className = "introImg" src={bg2} alt="introimg"/>
+        {/* <img  className = "introImg" src={bg1} alt="introimg"/> */}
         </div>
         <div className="content">
-          <p>Hi, I am a FreeLancer!!</p>
+          <p>Hii, I am a FreeLancer!!</p>
           <h1>React Developer</h1>
           <p className="subtitle">Crafting beautiful, responsive web experiences with modern React</p>
           <p className="description">Passionate about creating intuitive user interfaces and seamless user experiences. Specializing in React.js, JavaScript, and modern web technologies to build scalable applications that make a difference.</p>
           <div className="button-container">
-            <a href="/Resume.pdf" download className="resume-btn" target="_blank" >
+            <a href="/Resume_HS.pdf" download className="resume-btn" target="_blank" >
               <span>Resume <FaDownload /></span> 
             </a>
           </div>
--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,5 +1,6 @@
 import pr1 from "../Assets/img/pr1.jpg";
 import food from "../Assets/img/food.jpg";
+import Dash from "../Assets/img/Dash.png"
 
 const ProjectCardData = [
     {
@@ -15,7 +16,14 @@ const ProjectCardData = [
         text: "A smooth dining experience with online orders, reservations, and easy administration.",
         project: "https://foodfest-sage.vercel.app/",
         source: "https://github.com/harshsharma-02/resto"
+    },
+    {
+        imgsrc: Dash,
+        title: "Weather Trends & Forecast Analysis Dashboard",
+        text: "Built an interactive Power BI dashboard to analyze weather patterns, temperature trends, and key climate metrics using real-world data.",
+        project: "https://app.powerbi.com/groups/me/reports/6193d44b-cef2-4a63-9a8c-f9a6ee935103/5fe779cbed21791d87a9?ctid=3f1541fa-00a3-4097-ac36-46e2e50b31ce&experience=power-bi",
     }
+
 ];
 
 export default ProjectCardData;
